26ef53894ed35abdba174c0333b423c3d6558435
Hrithik <learningdemo00@gmail.com>
2025-12-14 00:13:26 +0530

    Shares multiple values between tasks

--- dags/xcom.py
+++ dags/xcom.py
@@ -1,18 +1,24 @@
-from airflow.sdk import task,dag,Context
+from airflow.sdk import task,dag
+from typing import Dict,Any
 
 @dag
 def my_dag():
 
         @task
-        def t1() ->int:
+        def t1() ->Dict[str,Any]:
                 val = 42
-                return val
+                my_sentenct = "Hello World"
+                return {
+                        "val" : val,
+                        "my_sentence" : my_sentenct
+                        }
 
         @task
-        def t2(val : int):
-                print(val)
+        def t2(data : Dict[str,Any]):
+                print(data['val'])
+                print(data['my_sentence'])
 
-        val = t1() 
-        t2(val)
+        values = t1() 
+        t2(values)
 
 my_dag()
