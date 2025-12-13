14dfa4d94daa6c3316680aef6cd5d77a4a9e24d6
Hrithik <learningdemo00@gmail.com>
2025-12-14 00:39:19 +0530

    Branching two task with same codition

--- dags/branch.py
+++ dags/branch.py
@@ -9,7 +9,7 @@ def branch():
         @task.branch
         def b(val):
                 if val==1:
-                        return "equal_to_1"
+                        return ["equal_to_1","different_one"]
                 return "not_equal_to_1"
         @task
         def equal_to_1():
@@ -17,8 +17,11 @@ def branch():
         @task
         def not_equal_to_1():
                 print('Not equal to 1')
+        @task
+        def different_one():
+                print("Someting else")
 
         val = a()
-        b(val) >> [equal_to_1(),not_equal_to_1()]
+        b(val) >> [equal_to_1(),not_equal_to_1(),different_one()]
 
 branch()
